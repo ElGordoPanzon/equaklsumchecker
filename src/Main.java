fc524f37b9512421eaf94d6bb5a398d728d57eb1
jose jimenez <jimenjos000@warren.k12.in.us>
2024-08-27 13:56:01 -0400

    erg

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+
+    private static boolean hasEqualSum(int num1, int num2, int num3) {
+        int variable = num1 + num2;
+        //find sum of num1 and num2 store it as variable
+        if (variable == num3){
+            return true;
+        }
+        //return whether that sume is equal to num3;
+        return false;
+    }
+
 }
